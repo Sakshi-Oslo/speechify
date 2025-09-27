accd2b3df5cd64e634c3aa9b7e30b961941d505e
Sakshi Agarwal <sakshiagarwal@Sakshis-MacBook-Air.local>
2025-09-27 15:36:03 +0530

    initial commit

---
+++ src/main/java/com/speechify/Client.java
@@ -0,0 +1,26 @@
+package com.speechify;
+
+public class Client {
+    private String id;
+    private String name;
+
+    public Client() {
+    }
+
+    public Client(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+} 
